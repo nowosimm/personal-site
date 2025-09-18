6d0da17776213de4def606fc163d1cd86991a60a
Maddie <nowosimm@mail.uc.edu>
2025-09-18 10:59:38 -0400

    tabler icons

---
+++ src/components/React Components/Icons.jsx
@@ -0,0 +1,10 @@
+// src/components/Icon.jsx
+import { IconArrowNarrowRight, IconArrowNarrowLeft, IconX, IconChevronLeft, IconChevronRight } from "@tabler/icons-react";
+
+export const icons = {
+  rightArrow: IconArrowNarrowRight,
+  leftArrow: IconArrowNarrowLeft,
+  exit: IconX,
+  chevronRight: IconChevronRight,
+  chevronLeft: IconChevronLeft
+};
