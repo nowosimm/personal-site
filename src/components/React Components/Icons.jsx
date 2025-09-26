8d98271a6fc7b330efbd29ba818d6b2ecf47f191
Maddie <nowosimm@mail.uc.edu>
2025-09-26 14:21:29 -0400

    styling and link updates

--- src/components/React Components/Icons.jsx
+++ src/components/React Components/Icons.jsx
@@ -1,10 +1,14 @@
 // src/components/Icon.jsx
-import { IconArrowNarrowRight, IconArrowNarrowLeft, IconX, IconChevronLeft, IconChevronRight } from "@tabler/icons-react";
+import { IconArrowNarrowRight, IconArrowNarrowLeft, IconX, IconChevronLeft, IconChevronRight, IconResize, IconAlignBoxLeftTop} from "@tabler/icons-react";
 
 export const icons = {
   rightArrow: IconArrowNarrowRight,
   leftArrow: IconArrowNarrowLeft,
   exit: IconX,
   chevronRight: IconChevronRight,
-  chevronLeft: IconChevronLeft
+  chevronLeft: IconChevronLeft,
+  resize: IconResize,
+  singlePage: IconAlignBoxLeftTop
+
+
 };
