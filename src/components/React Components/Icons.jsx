8c5306c592464612c565ce98b77b189432044325
Maddie <nowosimm@mail.uc.edu>
2025-10-13 20:39:42 -0400

    icons and colors

--- src/components/React Components/Icons.jsx
+++ src/components/React Components/Icons.jsx
@@ -1,5 +1,5 @@
 // src/components/Icon.jsx
-import { IconArrowNarrowRight, IconArrowNarrowLeft, IconX, IconChevronLeft, IconChevronRight, IconResize, IconAlignBoxLeftTop} from "@tabler/icons-react";
+import { IconArrowNarrowRight, IconArrowNarrowLeft, IconX, IconChevronLeft, IconChevronRight, IconResize, IconAlignBoxLeftTop, IconMail} from "@tabler/icons-react";
 
 export const icons = {
   rightArrow: IconArrowNarrowRight,
@@ -8,7 +8,8 @@ export const icons = {
   chevronRight: IconChevronRight,
   chevronLeft: IconChevronLeft,
   resize: IconResize,
-  singlePage: IconAlignBoxLeftTop
+  singlePage: IconAlignBoxLeftTop,
+  email: IconMail,
 
 
 };
