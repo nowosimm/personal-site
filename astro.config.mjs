68873d8c0e2d1449bce62d4d4004902ccc8f9230
Maddie <nowosimm@mail.uc.edu>
2025-10-14 19:56:23 -0400

    add publish.yaml

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import react from '@astrojs/react';
 // https://astro.build/config
 
 export default defineConfig({
+  site: 'https://nowosimm.github.io',
+  base: '/personal-site/',
   integrations: [
     react({
       experimentalDisableStreaming: true,
