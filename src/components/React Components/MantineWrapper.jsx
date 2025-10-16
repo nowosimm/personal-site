de76be98771166f7779995d1b9a98ccbfe46841e
Maddie <nowosimm@mail.uc.edu>
2025-10-16 10:27:52 -0400

    removing old files

--- src/components/React Components/MantineWrapper.jsx
+++
@@ -1,9 +0,0 @@
-import { MantineProvider } from '@mantine/core';
-
-export default function MantineWrapper({ children }) {
-  return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      {children}
-    </MantineProvider>
-  );
-}
