e750cca08468edadbf9255c81be33e0c7543c975
Maddie <nowosimm@mail.uc.edu>
2025-10-16 12:36:45 -0400

    click to copy email

--- src/components/React Components/Icons.jsx
+++ src/components/React Components/Icons.jsx
@@ -1,5 +1,5 @@
 // src/components/Icon.jsx
-import { IconArrowNarrowRight, IconArrowNarrowLeft, IconX, IconChevronLeft, IconChevronRight, IconResize, IconAlignBoxLeftTop, IconMail} from "@tabler/icons-react";
+import { IconArrowNarrowRight, IconArrowNarrowLeft, IconX, IconChevronLeft, IconChevronRight, IconResize, IconAlignBoxLeftTop, IconMail, IconCopy} from "@tabler/icons-react";
 
 export const icons = {
   rightArrow: IconArrowNarrowRight,
@@ -10,6 +10,7 @@ export const icons = {
   resize: IconResize,
   singlePage: IconAlignBoxLeftTop,
   email: IconMail,
+  copy: IconCopy,
 
 
 };
